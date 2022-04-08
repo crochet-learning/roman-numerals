ad110c3244992138544f56b0308a386f2024061b
crochetcode <fred.davison@outlook.com>
2022-04-08 14:27:59 -0500

    set up junit

--- src/test/java/com/linkedin/kata/RomanTests.java
+++ src/test/java/com/linkedin/kata/RomanTests.java
@@ -1,4 +1,11 @@
 package com.linkedin.kata;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class RomanTests {
+    @Test
+    public void junitWorks() {
+        assertTrue(true);
+    }
 }
